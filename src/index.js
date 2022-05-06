accbda4e976c9227698fecdfb97c1173a50908ea
Alex Luciano Roesler Rese <alexrese@outlook.com>
2022-05-06 10:09:29 -0300

    165 manipulando parametros

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+const { query } = require('express');
+const { request } = require('express');
 const { response } = require('express');
-const express = require('express');
+const express = require('uuidv4');
+
+const {uuid} = require
 
 const app = express();
+app.use(express.json()); // para recerver json
+
+const projects = [];
 
 app.get('/projects', (request, response) =>{
+    // const query = request.query; // todos os valores
+    const {title} = request.query; // parametros nomeados e desestruturados 
+    console.log(title);
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -11,7 +21,10 @@ app.get('/projects', (request, response) =>{
     ]);
 });
 
-app.put('/projects', (request, response) =>{
+app.put('/projects/:id', (request, response) =>{
+    // const params = query.params; //todos os parametros
+    const {id} = request.params; //parametros de rota
+    console.log(id);
     return response.json([
         'Project 5',
         'Project 4',
@@ -21,6 +34,8 @@ app.put('/projects', (request, response) =>{
 })
 
 app.post('/projects', (request, response) => {
+    const body = request.body;
+    console.log(body);
     return response.json([
         'Projeto 1',
         'Projeto 2',
